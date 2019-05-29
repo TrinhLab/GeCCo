23b4759b88df00c388d7f9a1ea6c3421cb4beef3
Sergio Garcia <sergiogarciamf@gmail.com>
2019-05-29 14:07:16 -0400

    rerun ylproteomics problem

--- problems/ylproteomics/run_gec.sh
+++ problems/ylproteomics/run_gec.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gec P3_00vsP3_400/ -p param.csv -n network/network.csv  --write_scores true
-gec P3_00vsWT_00/ -p param.csv -n network/network.csv  --write_scores true
-gec WT_00vsWT_400/ -p param.csv -n network/network.csv --write_scores true
+gec P3_00vsP3_400/ -p param.csv -n network/network.csv  --write_scores true --include_no_change true
+gec P3_00vsWT_00/ -p param.csv -n network/network.csv  --write_scores true --include_no_change true
+gec WT_00vsWT_400/ -p param.csv -n network/network.csv --write_scores true --include_no_change true
