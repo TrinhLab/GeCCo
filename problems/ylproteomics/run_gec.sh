7260fa394216a038433818548ae735b31832459b
Sergio Garcia <sergiogarciamf@gmail.com>
2019-06-05 13:36:48 -0400

    fix: add missing files for previous commit

--- problems/ylproteomics/run_gec.sh
+++ problems/ylproteomics/run_gec.sh
@@ -3,3 +3,4 @@
 gec P3_00vsP3_400/ -p param.csv -n network/network.csv  --write_scores true --include_no_change true
 gec P3_00vsWT_00/ -p param.csv -n network/network.csv  --write_scores true --include_no_change true
 gec WT_00vsWT_400/ -p param.csv -n network/network.csv --write_scores true --include_no_change true
+gec P3_400vsWT_400/ -p param.csv -n network/network.csv --write_scores true --include_no_change true
